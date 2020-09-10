7037d235f184f7ad3c9c441a6b35dd15406e8392
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2020-09-09 23:30:39 -0500

    Add placeholder for post authentication trigger

---
+++ src/LaughAndGroan.Actions/Users/PostAuthenticationHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace LaughAndGroan.Actions.Users
+{
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using Amazon.Lambda.Core;
+
+    public class PostAuthenticationHandler
+    {
+        readonly UsersService _users = new UsersService();
+
+        public async Task<JsonElement> CreateUserAfterAuthentication(JsonElement request, ILambdaContext context)
+        {
+            // example request input
+            /*
+                {
+                  "version": "string",
+                  "triggerSource": "string",
+                  "region": "AWSRegion",
+                  "userPoolId": "string",
+                  "userName": "string",
+                  "callerContext": {
+                    "awsSdkVersion": "string",
+                    "clientId": "string"
+                  },
+                  "request": {
+                    "userAttributes": {
+                      "string": "string"
+                    },
+                    "newDeviceUsed": true,
+                    "clientMetadata": {
+                      "string": "string"
+                    }
+                  },
+                  "response": {}
+                }
+            */
+            
+            // TODO: create user
+
+            return request;
+        }
+    }
+}
