c1e1f1faaf8845fa1e00cffcd84dd852d12de98a
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2021-06-06 10:56:57 -0500

    Manage local DynamoDB for testing

--- psakefile.ps1
+++ psakefile.ps1
@@ -12,22 +12,47 @@ properties {
 }
 
 task default -depends Test
-task CI -depends Clean, Test, Publish -description "Continuous Integration process"
+task CI -depends Clean, Restore, Test, Publish -description "Continuous Integration process"
 task Rebuild -depends Clean, Compile -description "Rebuild the code and database, no testing"
 
 task Info -description "Display runtime information" {
     exec {
         dotnet --info
+        docker version
         write-host "node: $(& node --version)"
         write-host "npm: $(& npm --version)"
         write-host "Version: $version"
     }
 }
 
+task SetupLocalDynamoDb -depends Info -description "Setup local DynamoDb for testing" {
+    exec { docker-compose -p laugh-and-groan-local -f local-dynamodb.docker-compose.yml up -d } -workingDirectory test
+}
+
+task TearDownLocalDynamoDb -depends Info -description "Tear down local DynamoDb for testing" {
+    exec { docker-compose -p laugh-and-groan-local -f local-dynamodb.docker-compose.yml down } -workingDirectory test
+}
+
+task StartLocalDynamoDb -depends Info -description "Start local DynamoDb for testing" {
+    exec { docker-compose -p laugh-and-groan-local -f local-dynamodb.docker-compose.yml start } -workingDirectory test
+}
+
+task StopLocalDynamoDb -depends Info -description "Stop local DynamoDb for testing" {
+    exec { docker-compose -p laugh-and-groan-local -f local-dynamodb.docker-compose.yml stop } -workingDirectory test
+}
+
 task Test -depends Compile -description "Run unit tests" {
     get-childitem . test/*.Tests -directory | foreach-object {
         exec { dotnet fixie --configuration $configuration --no-build } -workingDirectory $_.fullname
     }
+}
+
+task Restore -depends Info -description "Restore all packages for build" {
+    exec { dotnet restore }
+    exec { npm install } -workingDirectory src/laugh-and-groan-website
+    exec { npm install } -workingDirectory deploy/Certificates
+    exec { npm install } -workingDirectory deploy/HostedZones
+    exec { npm install } -workingDirectory deploy/LaughAndGroan
 }
   
 task Compile -depends Info -description "Compile the solution" {
