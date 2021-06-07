e10912b145008a338ab13a9ac6935ed3ee211993
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2021-06-06 23:04:16 -0500

    Implement controllers

--- src/LaughAndGroan.Api/Features/Posts/PostsController.cs
+++ src/LaughAndGroan.Api/Features/Posts/PostsController.cs
@@ -1,24 +1,69 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LaughAndGroan.Api.Features.Posts
+﻿namespace LaughAndGroan.Api.Features.Posts
 {
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
     using System.Net.Mime;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     [Route("posts")]
     [ApiController]
     public class PostsController : ControllerBase
     {
-        [HttpPost(""), Produces(MediaTypeNames.Application.Json)]
-        public async Task<ActionResult<PostData>> Create([FromBody] PostApiRequest request)
+        private readonly PostsService _posts;
+
+        public PostsController(PostsService posts)
         {
-            return Ok(new PostData());
+            _posts = posts;
         }
 
+        [HttpPost(""), Consumes(MediaTypeNames.Application.Json), Produces(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<PostData>> Create([FromBody] PostApiRequest request)
+        {
+            var userId = HttpContext.User.ExtractUserId();
+            if (userId == null)
+                return Unauthorized();
 
+            var postCreated = await _posts.CreatePost(userId, request.Url, request.Title);
+            
+            return Ok(postCreated);
+        }
+
+        [HttpGet(""), Produces(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<GetPostsResponse>> GetPosts([FromQuery(Name = "by")] string userName,
+            [FromQuery(Name = "from")] string fromPostId, CancellationToken cancellationToken)
+        {
+            var result = await _posts.GetPosts(fromPostId, userName == null ? null : new[] { userName }, cancellationToken: cancellationToken);
+            var response = new GetPostsResponse()
+            {
+                Data = result.Take(25).Select(p => new PostApiResponse(p)).ToArray()
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("{postId}"), Produces(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<PostApiResponse>> GetPost([FromRoute] string postId,
+            CancellationToken cancellationToken)
+        {
+            var postFound = await _posts.GetPost(postId, cancellationToken);
+            if (postFound == null)
+                return NotFound();
+
+            return Ok(postFound);
+        }
+
+        [HttpDelete("{postId}")]
+        public async Task<ActionResult> DeletePost([FromRoute] string postId, CancellationToken cancellationToken)
+        {
+            var userId = HttpContext.User.ExtractUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            if (!await _posts.DeletePost(userId, postId, cancellationToken))
+                return Unauthorized();
+
+            return NoContent();
+        }
     }
 }
---
+++ src/LaughAndGroan.Api/Features/Users/UsersController.cs
@@ -0,0 +1,34 @@
+﻿namespace LaughAndGroan.Api.Features.Users
+{
+    using System.Net.Mime;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersService _users;
+
+        public UsersController(UsersService users)
+        {
+            _users = users;
+        }
+
+        [HttpGet("me"), Produces(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<UserApiData>> GetMe(CancellationToken cancellationToken)
+        {
+            var userId = HttpContext.User.ExtractUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            var response = await _users.GetById(userId, cancellationToken);
+            return Ok(new UserApiData
+            {
+                UserName = response.UserName,
+                Id = response.UserId
+            });
+        }
+    }
+}
