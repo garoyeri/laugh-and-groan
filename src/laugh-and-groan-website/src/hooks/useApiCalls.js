5e2e6f2fd5031752b1a0407184b522739f48c3b3
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2021-06-07 00:38:47 -0500

    Use API domain

--- src/laugh-and-groan-website/src/hooks/useApiCalls.js
+++ src/laugh-and-groan-website/src/hooks/useApiCalls.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-const apiDomain = "https://localhost:5001";
+const apiDomain = "https://api.laughandgroan.com";
 
 const useApiCalls = () => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
