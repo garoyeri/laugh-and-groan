f4ef43e24956f9aef688b3791d8ce6258bb1519d
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2021-06-08 21:58:21 -0500

    Use new high level construct authorizer

--- deploy/LaughAndGroan/lib/apigateway.ts
+++ deploy/LaughAndGroan/lib/apigateway.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as api from "@aws-cdk/aws-apigatewayv2";
 import * as api_int from "@aws-cdk/aws-apigatewayv2-integrations";
+import * as api_auth from "@aws-cdk/aws-apigatewayv2-authorizers";
 import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as route53 from "@aws-cdk/aws-route53";
 import { Lambdas } from "./lambdas";
@@ -55,38 +56,44 @@ export class ApiGateway extends cdk.Construct {
       }),
     });
 
-    let routes: api.HttpRoute[] = [];
-
-    // Proxy for ASP.NET Core
-    routes = routes.concat(
-      gateway.addRoutes({
-        path: "/{proxy+}",
-        methods: [api.HttpMethod.ANY],
-        integration: new api_int.LambdaProxyIntegration({
-          handler: props.lambdas.aspnetLambda,
-        })
-      })
-    )
-
+    let jwtAuthorizer: api.IHttpRouteAuthorizer = new api.HttpNoneAuthorizer();
     if (props.authIssuer && props.authClientId) {
-      const authorizer = new api.CfnAuthorizer(this, "ApiGatewayAuthorizer", {
-        name: "ApiGatewayAuthorizer",
-        apiId: gateway.httpApiId,
-        authorizerType: "JWT",
-        identitySource: ["$request.header.Authorization"],
-        jwtConfiguration: {
-          audience: [props.authClientId],
-          issuer: props.authIssuer,
-        },
-      });
-
-      // https://dev.to/martzcodes/token-authorizers-with-apigatewayv2-tricks-apigwv1-doesn-t-want-you-to-know-41jn
-      routes.forEach((route) => {
-        const routeCfn = route.node.defaultChild as api.CfnRoute;
-        routeCfn.authorizerId = authorizer.ref;
-        routeCfn.authorizationType = "JWT";
+      jwtAuthorizer = new api_auth.HttpJwtAuthorizer({
+        jwtAudience: [props.authClientId],
+        jwtIssuer: props.authIssuer
       });
     }
+    const noneAuthorizer = new api.HttpNoneAuthorizer();
+
+    // Proxy for ASP.NET Core
+    gateway.addRoutes({
+      path: "/api/{proxy+}",
+      methods: [api.HttpMethod.ANY],
+      integration: new api_int.LambdaProxyIntegration({
+        handler: props.lambdas.aspnetLambda,
+      }),
+      authorizer: jwtAuthorizer || noneAuthorizer
+    });
+
+    // Swagger route
+    gateway.addRoutes({
+      path: "/swagger/{proxy+}",
+      methods: [api.HttpMethod.ANY],
+      integration: new api_int.LambdaProxyIntegration({
+        handler: props.lambdas.aspnetLambda,
+      }),
+      authorizer: noneAuthorizer
+    });
+
+    // Health check route
+    gateway.addRoutes({
+      path: "/health",
+      methods: [api.HttpMethod.ANY],
+      integration: new api_int.LambdaProxyIntegration({
+        handler: props.lambdas.aspnetLambda,
+      }),
+      authorizer: noneAuthorizer
+    });
 
     new cdk.CfnOutput(this, "ApiUrl", {
       value: gateway.url ?? "",
